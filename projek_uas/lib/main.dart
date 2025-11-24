526bd9acc172133982c77e86fde1db93cc672c46
Deadring <81697605+Deadring@users.noreply.github.com>
2025-11-24 17:29:37 +0700

    Update main function and app structure
    Refactor main function to initialize Firebase and theme provider.

--- projek_uas/lib/main.dart
+++ projek_uas/lib/main.dart
@@ -1,36 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:projek_uas/screen/add_post.dart';
+import 'package:projek_uas/screen/sign_up_screen.dart';
 import 'package:projek_uas/screen/sign_in_screen.dart';
+import 'package:projek_uas/screen/splash_screen.dart';
 import 'package:projek_uas/screen/home_screen.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'package:projek_uas/screen/theme_provider.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  final themeProvider = ThemeProvider();
+  await themeProvider.loadTheme(); // ambil tema dari SharedPreferences
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider<ThemeProvider>(
+          create: (_) => themeProvider,
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  Future<bool> isLoggedIn() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.containsKey('email');
-  }
-
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+
     return MaterialApp(
-      title: 'Login Demo',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: FutureBuilder<bool>(
-        future: isLoggedIn(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          }
-          return snapshot.data == true
-              ? const HomeScreen()
-              : const SignInScreen();
-        },
-      ),
+      title: 'Wisata Anywhere',
+
+      themeMode: themeProvider.themeMode,
+      theme: ThemeData.light(),
+      darkTheme: ThemeData.dark(),
+
+      routes: {
+        '/login': (context) => const SignInScreen(),
+        '/home': (context) => const HomeScreen(),
+        '/add': (context) => const AddPostScreen(),
+      },
+
+      home: const SplashScreen(),
     );
   }
 }
